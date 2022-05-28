25922bfa147575dc7a704aedddd68549f482d403
BombayV <OMGmsebastian@gmail.com>
2022-05-28 14:30:58 -0400

    feat(notification): setup bg color

--- web/src/main.js
+++ web/src/main.js
@@ -5,7 +5,6 @@ import './index.css'
 
 const themeOverrides = {
   Message: {
-    "color": "rgba(8, 8, 197, 1)",
     "colorError": "rgba(125, 76, 76, 1)",
     "colorSuccess": "rgba(84, 153, 105, 1)",
     "textColor": "rgba(255, 255, 255, 1)",
@@ -17,6 +16,9 @@ const themeOverrides = {
     "colorInfo": "rgba(60, 82, 112, 1)",
     "colorWarning": "rgba(156, 139, 71, 1)",
     "colorLoading": "rgba(123, 123, 123, 1)"
+  },
+  "Notification": {
+    "color": "rgba(75, 85, 99, 1)"
   }
 }
 
